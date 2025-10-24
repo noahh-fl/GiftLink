d1ac2ff64ec4bcfe3021f5b17830552e47995aae
noahh-fl <18nflewelling@gmail.com>
2025-10-24 02:23:35 -0700

    chore(setup): add HomeChoice page and router setup

---
+++ src/pages/HomeChoice.tsx
@@ -0,0 +1,11 @@
+export default function HomeChoice() {
+  return (
+    <main>
+      <h1>Start a Space</h1>
+      <p>Create a new list or join an existing one.</p>
+
+      <button>Create New List</button>
+      <button>Join Active List</button>
+    </main>
+  );
+}
